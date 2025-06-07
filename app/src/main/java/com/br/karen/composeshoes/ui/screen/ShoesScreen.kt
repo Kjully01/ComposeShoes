d985c5b41f0606a7d6f2e29493211d6baf3fe40f
Karen Jully <karenjully.kr15@gmail.com>
2025-06-07 12:13:19 -0300

    Initial commit - Definição das cores do tema

---
+++ app/src/main/java/com/br/karen/composeshoes/ui/screen/ShoesScreen.kt
@@ -0,0 +1,2 @@
+package com.br.karen.composeshoes.ui.screen
+
---
+++ app/src/main/java/com/br/karen/composeshoes/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.br.karen.composeshoes.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Black = Color(0xFF000000)
+val DarkGray = Color(0xFF424242)
+val MediumGray = Color(0xFF757575)
+
+val Gray = Color(0xFF9E9E9E)
+val LightGray = Color(0xFFBDBDBD)
+
+val White = Color(0xFFFFFFFF)
+
+val Orange = Color(0xFFFF8D41)
+val Amber = Color(0xFFE6A200)
